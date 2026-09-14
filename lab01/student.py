1b507603a7ded13e688d749e071ae8211e7f39fd
Ben Smith <C00321354@setu.ie>
2026-09-14 10:59:38 +0000

    Complete Lab 1

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+id = input("What is your student number? ")
+year = input("What year are you in? ")
+
+print("Name: "+ name)
+print("Student Number: "+ id)
+print("Year: "+ year)
